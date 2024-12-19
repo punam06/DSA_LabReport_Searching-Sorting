56a50b51d3658724fe428b3606f0399d6a2042f7
punam06 <144909093+punam06@users.noreply.github.com>
2024-12-20 01:33:28 +0600

    Create Execrcise_1.cpp

---
+++ Execrcise_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Maximum = " << max << ", Minimum = " << min << endl;
+
+    return 0;
+}
